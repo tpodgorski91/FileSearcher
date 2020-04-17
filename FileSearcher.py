99e2ba395fe487e66b5d4ac347197def25245e43
Your Name <you@example.com>
2020-04-17 17:34:57 +0200

    open looked txt file in notepad from "D:\" drive

--- FileSearcher.py
+++ FileSearcher.py
@@ -1,21 +1,24 @@
-import glob
-#TODO open file in another application e.g. Notepad
-#TODO print out only file name without path
-#TODO make list of files more readable
+import glob, subprocess
 
-print("Enter file name")
-filename = input()
+#TODO: run program automatically
 
-files = glob.glob("D:\\*\\" + filename + "*")
+files=[]
+while not files:
+    print("Please enter txt file name")
+    filename = input()
+    files = glob.glob("D:\\*\\*" + filename + "*.txt")
 
 for file in range(len(files)):
     print(str(file) + "  " + files[file])
 
-print("Plz enter file number from the list above")
+print("Please enter row index")
 number = int(input())
-if number > len(files):
-    print("Plz enter number equal to/ lower than " + str(len(files)-1))
-tekst = open(files[number],"r")
-content = tekst.read()
-print(content)
 
+try:
+    while number < 0 or number > len(files)-1:
+        print("Plz enter index no higher than " + str(len(files) - 1))
+        number = int(input())
+except IndexError:
+    print("Index is out of range")
+
+subprocess.call([r"notepad.exe", files[number]])
