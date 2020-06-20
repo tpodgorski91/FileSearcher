f8abb7efdc4642febe86bfe0c79cb7aaa7ac90b4
Your Name <you@example.com>
2020-06-20 17:10:28 +0200

    code under review:
    added methods for different OS
    if main condition statement added
    returns Path instead of string

--- file_searcher.py
+++ file_searcher.py
@@ -1,9 +1,38 @@
-import string
-from ctypes import windll
 from pathlib import Path, PurePath
+import platform
+from typing import List
+import subprocess
 
 
-def get_drives():
+def list_drives():
+    system_name = platform.system()
+
+    method = {
+        "Linux": list_drives_linux,
+        "Darwin": list_drives_macos,
+        "Windows": list_drives_windows,
+    }[system_name]
+
+    drives = method()
+    return drives
+
+
+def list_drives_linux() -> List[str]:
+    # TODO: find out how to return
+    return []
+
+
+def list_drives_macos() -> List[str]:
+    import psutil
+    return [
+        partition.device for partition in psutil.disk_partitions()
+    ]
+
+
+def list_drives_windows() -> List[str]:
+    import string
+    from ctypes import windll
+
     drives = []
     bitmask = windll.kernel32.GetLogicalDrives()
     for letter in string.ascii_uppercase:
@@ -13,15 +42,17 @@ def get_drives():
 
     return drives
 
+# TODO:how to make WindowsPath subprocess.call method  readable
+
 
 if __name__ == '__main__':
-    for element in get_drives():
-        print(element)    # On my PC, this prints ['A', 'C', 'D', 'F', 'H']
-    print("")
-    print("Please type drive letter from the list above (e.g. D)")
-    user_driver = input()
-    print("Please provide file name")
+    print(list_drives())
+    print("Please type from the list above drive where file is stored")
+    user_drive = input()
+    print("Please provide either entire or some portion of file name")
     file_name = input()
-    p = sorted(Path(f"{user_driver}:/").glob(f"*/*{file_name}*.txt"))
-    f = open(p[0])
-    print(f.read())
+    p = sorted(Path(f"{user_drive}:/").glob(f"*/*{file_name}*.txt"))
+    print(p)
+    subprocess.call([r"notepad.exe", p])
+    # f = open(p[0])
+    # print(f.read())
