d4e3874f65b93981ce1d803ed87491803f461d71
Your Name <you@example.com>
2020-07-02 21:11:06 +0200

    enables to choose file from list by index test does it open file in different os

--- file_searcher.py
+++ file_searcher.py
@@ -1,47 +1,9 @@
 from pathlib import Path
 from typing import List
+import platform
+import os
 import subprocess
 
-
-# def list_drives():
-#     system_name = platform.system()
-#
-#     method = {
-#         "Linux": list_drives_linux,
-#         "Darwin": list_drives_macos,
-#         "Windows": list_drives_macos,
-#     }[system_name]
-#
-#     drives = method()
-#     return drives
-#
-#
-# def list_drives_linux() -> List[str]:
-#     # TODO: find out how to return
-#     return []
-#
-#
-# def list_drives_macos() -> List[str]:
-#     import psutil
-#     return [
-#         partition.device for partition in psutil.disk_partitions()
-#     ]
-#
-
-# def list_drives_windows() -> List[str]:
-#     import string
-#     from ctypes import windll
-#
-#     drives = []
-#     bitmask = windll.kernel32.GetLogicalDrives()
-#     for letter in string.ascii_uppercase:
-#         if bitmask & 1:
-#             drives.append(letter)
-#         bitmask >>= 1
-#
-#     return drives
-
-# TODO: return list with index
 
 def list_drives() -> List[str]:
     import psutil
@@ -53,36 +15,27 @@ def list_drives() -> List[str]:
     return drives
 
 
-# TODO: print exact file index from the received list, add header : "index file path"
 def get_file(drive, name):
     """
     :param drive:
     :param name:
     :return: print list of files that names are matching with pattern
     """
-
+    files_list = []
     file_path = sorted(Path(drive).rglob(f'*{name}*.*'))
-    for file in file_path:
-        print(file_path.index(file), file)
-    # file_path = sorted(Path(f"{drive}").glob(f"*/*{name}*"))
-    # file_path = str(file_path)
-    # if platform.system() == 'Windows':
-    #     file_path = file_path[14:-3]
-    # else:
-    #     file_path = file_path[11:-3]
-    # return file_path
-
-
-def get_txt_editor():
-    import platform
-    system_name = platform.system()
-    result = {
-            "Linux": r'random',
-            "Darwin": r'random',
-            "Windows": r'notepad.exe',
-
-    }[system_name]
-    return result
+    for file_loc in file_path:
+        print(file_path.index(file_loc), file_loc)
+        file_loc = str(file_loc)
+        files_list.append(file_loc)
+    print("Choose index.")
+    list_index = int(input())
+    if platform.system() == "Windows":
+        os.startfile(file_path[list_index])
+    elif platform.system() == "Darwin":
+        subprocess.Popen(["open", file_path[list_index]])
+    else:
+        subprocess.Popen(["xdg-open", file_path[list_index]])
+    return file_path[list_index]
 
 
 if __name__ == '__main__':
@@ -94,4 +47,3 @@ if __name__ == '__main__':
     print("Please provide either entire or portion of text file name.")
     file_name = input()
     get_file(user_drive, file_name)
-    # subprocess.call([get_txt_editor(), get_file(user_drive, file_name)])
