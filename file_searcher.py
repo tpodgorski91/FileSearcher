3f9dce5ee765b3edd01d666ba90a4f8d46fa5f09
Your Name <you@example.com>
2020-07-10 12:08:24 +0200

    enables listing linux drives appropriately

--- file_searcher.py
+++ file_searcher.py
@@ -5,10 +5,35 @@ import os
 import subprocess
 
 
-def list_drives() -> List[str]:
+def list_drives():
+    system_name = platform.system()
+    method = {
+        "Linux": list_drives_linux,
+        "Darwin": list_drives_win_mac,
+        "Windows": list_drives_win_mac,
+    }[system_name]
+    drives = method()
+    return drives
+
+
+def list_drives_linux() -> List[str]:
     """
 
-    :return: list of user drives
+    :return: list of user drives on Linux
+    """
+    import psutil
+    drives = [
+        partition.mountpoint for partition in psutil.disk_partitions()
+    ]
+    for drive in drives:
+        print(drive)
+    return drives
+
+
+def list_drives_win_mac() -> List[str]:
+    """
+
+    :return: list of user drives on either Windows or macOS
     """
     import psutil
     drives = [
@@ -22,15 +47,15 @@ def list_drives() -> List[str]:
 def user_input():
     """
 
-    :return: drive name and file name
+    :return: list of files and search pattern match
     """
-    print("Please choose from above one valid drive where text file is stored and type it below."
+    print("Please choose from above one valid drive where file is stored and type it below."
           "\nChoice should be exactly the same as one from above.")
     drive_name = input()
-    print("Please provide either entire or portion of text file name.")
+    print("Please provide either entire or portion of file name.")
     file_name = input()
     files_list = []
-    print(drive_name, file_name)
+    # print(drive_name, file_name)
     file_path = sorted(Path(drive_name).rglob(f'*{file_name}*.*'))
     for file_loc in file_path:
         print(file_path.index(file_loc), file_loc)
@@ -44,9 +69,13 @@ def user_input():
 
 
 def choose_index():
+    """
+
+    :return: opens chosen file from the list base on list index
+    """
     try:
         print("Choose index.")
-        print(look_for_file)
+        # print(look_for_file)
         list_index = int(input())
         if platform.system() == "Windows":
             os.startfile(look_for_file[list_index])
